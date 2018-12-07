d5d348b6e10f325d2a51a661d315bb4c4ce102b3
Eike Siewertsen <eike@technocreatives.com>
2018-12-07 14:06:48 +0100

    rustfmt

--- src/targets/osx.rs
+++ src/targets/osx.rs
@@ -1,34 +1,66 @@
 use std::fs::{self, File};
-use std::path::{Path, PathBuf};
 use std::io::Write;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 static APP_NAME: &str = "MacDivvun";
 
-pub fn create_installer(bcp47code: &str, version: &str, build: u64, zhfst_file: &Path, output_dir: &Path) {
-	let package = "no.divvun.MacDivvun";
+pub fn create_installer(
+    bcp47code: &str,
+    version: &str,
+    build: u64,
+    zhfst_file: &Path,
+    output_dir: &Path,
+) {
+    let package = "no.divvun.MacDivvun";
     let bundle_name = format!("{}.{}.bundle", package, bcp47code);
 
-	create_bundle(&bundle_name, package, bcp47code, version, build, zhfst_file, output_dir);
-	create_installer_from_bundle(APP_NAME, &output_dir.join(bundle_name), version, package, output_dir);
+    create_bundle(
+        &bundle_name,
+        package,
+        bcp47code,
+        version,
+        build,
+        zhfst_file,
+        output_dir,
+    );
+    create_installer_from_bundle(
+        APP_NAME,
+        &output_dir.join(bundle_name),
+        version,
+        package,
+        output_dir,
+    );
 }
 
-pub fn create_bundle(bundle_name: &str, package: &str, bcp47code: &str, version: &str, build: u64, zhfst_file: &Path, output_dir: &Path) {
+pub fn create_bundle(
+    bundle_name: &str,
+    package: &str,
+    bcp47code: &str,
+    version: &str,
+    build: u64,
+    zhfst_file: &Path,
+    output_dir: &Path,
+) {
     fs::create_dir_all(output_dir).expect("output dir to be created");
     let bundle_dir = output_dir.join(&bundle_name);
     match fs::remove_dir_all(&bundle_dir) {
-        Ok(_) => {},
-        Err(ref err) if err.kind() == std::io::ErrorKind::NotFound => {},
-        Err(err) => panic!(err)
+        Ok(_) => {}
+        Err(ref err) if err.kind() == std::io::ErrorKind::NotFound => {}
+        Err(err) => panic!(err),
     };
 
     let content_path = bundle_dir.join("Contents");
     let resources_path = content_path.join("Resources");
     fs::create_dir_all(&resources_path).expect("bundle dir to be created");
-    fs::copy(zhfst_file, resources_path.join("speller.zhfst")).expect("zhfst file to copy successfully");
+    fs::copy(zhfst_file, resources_path.join("speller.zhfst"))
+        .expect("zhfst file to copy successfully");
 
-    let mut plist_file = File::create(content_path.join("Info.plist")).expect("plist file to be created");
-    plist_file.write_all(make_plist(bcp47code, version, build, APP_NAME, package).as_bytes()).expect("plist to be written");
+    let mut plist_file =
+        File::create(content_path.join("Info.plist")).expect("plist file to be created");
+    plist_file
+        .write_all(make_plist(bcp47code, version, build, APP_NAME, package).as_bytes())
+        .expect("plist to be written");
 }
 
 fn make_plist(bcp47code: &str, version: &str, build: u64, app_name: &str, package: &str) -> String {
@@ -76,60 +108,74 @@ fn make_plist(bcp47code: &str, version: &str, build: u64, app_name: &str, packag
 "#, package = package, bcp47code = bcp47code, app_name = app_name, version = version, build = build)
 }
 
-fn create_installer_from_bundle(app_name: &str, bundle_dir: &Path, version: &str, package: &str, output_dir: &Path) {
-	let component_pkg_name = create_component_package(output_dir, bundle_dir, package, version);
-	
-	let distribution_path = output_dir.join("distribution.xml");
-	{
-		let mut distribution_file = File::create(&distribution_path).expect("distribution file to be created");
-		distribution_file.write_all(make_distribution(&app_name, package, &component_pkg_name).as_bytes()).expect("distribution file to be written");
-	}
+fn create_installer_from_bundle(
+    app_name: &str,
+    bundle_dir: &Path,
+    version: &str,
+    package: &str,
+    output_dir: &Path,
+) {
+    let component_pkg_name = create_component_package(output_dir, bundle_dir, package, version);
 
-	let pkg_name = format!("{}.unsigned.pkg", package);
+    let distribution_path = output_dir.join("distribution.xml");
+    {
+        let mut distribution_file =
+            File::create(&distribution_path).expect("distribution file to be created");
+        distribution_file
+            .write_all(make_distribution(&app_name, package, &component_pkg_name).as_bytes())
+            .expect("distribution file to be written");
+    }
 
-	let result = Command::new("productbuild")
-		.current_dir(output_dir)
-		.arg("--distribution")
-		.arg(distribution_path)
-		.arg("--version")
-		.arg(version)
-		// .arg("--package-path")
-		// .arg(output_dir)
-		.arg(pkg_name)
-		.status()
-		.expect("productbuild to execute successfully");
-	
-	if !result.success() {
-		panic!("productbuild failed");
-	}
+    let pkg_name = format!("{}.unsigned.pkg", package);
+
+    let result = Command::new("productbuild")
+        .current_dir(output_dir)
+        .arg("--distribution")
+        .arg(distribution_path)
+        .arg("--version")
+        .arg(version)
+        // .arg("--package-path")
+        // .arg(output_dir)
+        .arg(pkg_name)
+        .status()
+        .expect("productbuild to execute successfully");
+
+    if !result.success() {
+        panic!("productbuild failed");
+    }
 }
 
-fn create_component_package(output_dir: &Path, bundle_dir: &Path, package: &str, version: &str) -> String {
-	let pkg_name = format!("{}.pkg", package);
+fn create_component_package(
+    output_dir: &Path,
+    bundle_dir: &Path,
+    package: &str,
+    version: &str,
+) -> String {
+    let pkg_name = format!("{}.pkg", package);
 
-	let result = Command::new("pkgbuild")
-		.current_dir(output_dir)
-		.arg("--component")
+    let result = Command::new("pkgbuild")
+        .current_dir(output_dir)
+        .arg("--component")
         .arg(bundle_dir)
-		.arg("--ownership")
-		.arg("recommended")
-		.arg("--install-location")
-		.arg("/Library/Keyboard Layouts") // TODO: where
-		.arg("--version")
-		.arg(version)
-		.arg(&pkg_name)
-		.status()
-		.expect("pkgbuild to complete successfully");
-	
-	if !result.success() {
-		panic!("pkgbuild failed");
-	}
-	
-	pkg_name
+        .arg("--ownership")
+        .arg("recommended")
+        .arg("--install-location")
+        .arg("/Library/Services")
+        .arg("--version")
+        .arg(version)
+        .arg(&pkg_name)
+        .status()
+        .expect("pkgbuild to complete successfully");
+
+    if !result.success() {
+        panic!("pkgbuild failed");
+    }
+
+    pkg_name
 }
 
 fn make_distribution(app_name: &str, package: &str, component_package: &str) -> String {
-	format!(r#"<?xml version="1.0" encoding="UTF-8"?>
+    format!(r#"<?xml version="1.0" encoding="UTF-8"?>
 <installer-gui-script minSpecVersion="2">
 	<title>{app_name}</title>
 	<options customize="never" rootVolumeOnly="true"/>
@@ -139,7 +185,8 @@ fn make_distribution(app_name: &str, package: &str, component_package: &str) -> 
     	</line>
   	</choices-outline>
 
-	<choice id="default">
+	<choice id="default" />
+    <choice id="{package}" visible="false">
 		<pkg-ref id="{package}"/>
 	</choice>
 
